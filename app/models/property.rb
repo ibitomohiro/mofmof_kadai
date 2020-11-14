7fbf7ca1a05da20b9a813224cb63496373d711a8
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-11-14 15:37:54 +0900

    reject_ifを追記

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,10 +1,10 @@
 class Property < ApplicationRecord
   has_many :closest_stations, dependent: :destroy
-  accepts_nested_attributes_for :closest_stations 
+  accepts_nested_attributes_for :closest_stations, allow_destroy: true, reject_if: :all_blank
   validates :property_name, presence: true
   validates  :rent, presence: true
   validates :address, presence:true
   validates :age, presence: true
-  validates :note, length: { maximum:140 }, allow_blank: true
+  validates :note, length: { maximum:300 }, allow_blank: true
 
 end
